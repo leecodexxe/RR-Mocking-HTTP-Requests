8153461e40cbb999c7101fe50e64c2adca5f0fdf
leecodexxe~ <lee4works@gmail.com>
2023-08-03 21:36:09 -0700

    change for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
+import {render, screen, waitFor} from '@testing-library/react'
+import App from './App'
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
+
+
+test("received GitHub name from API using mocked network event", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Freeple McGee'}))
+    render(<App />)
+    const headingEl = await waitFor(() => screen.getByRole('heading', {level: 2}))
+    expect(headingEl.textContent).toBe('Freeple McGee')
+})
 
 
